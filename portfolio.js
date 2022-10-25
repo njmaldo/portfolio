5ecc226a3721cd6e090ee52a139fd2637fbb4d48
nestor Maldonado <nestorjmaldo@gmail.com>
2022-10-25 15:07:42 -0300

    Header, dark, contacto ,footer modificado

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#menu_bar');
+let btnSwitch = document.querySelector('#switch');
+
+menu_bar.addEventListener('click', () => {
+    menu.classList.toggle('menu_toggle');
+    setTimeout(toggle, 5000);
+});
+const toggle = () => {
+    location.reload();
+}
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
+
